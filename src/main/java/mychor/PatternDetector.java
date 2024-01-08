909c848f1e82e98b8580f06f95816118d00be3f1
anavarr <artnavarro@protonmail.com>
2024-01-08 12:36:18 +0100

    start implementing pattern detection

---
+++ src/main/java/mychor/PatternDetector.java
@@ -0,0 +1,42 @@
+package mychor;
+
+import java.util.List;
+
+public class PatternDetector {
+
+    CompilerContext ctx;
+
+    List<Session> patterns;
+
+    static final String REST_PATTERN =
+            "client -> server;" +
+            "server -> client";
+
+    static final String REACTIVE_MESSAGING =
+            "subscriber -> producer ; " +
+            "producer -> subscriber; " +
+            "(subscriber -> producer | producer -> subscriber)*";
+
+    static final String gRPC_un_un =
+            "client -> server;" +
+            "server -> client";
+    static final String gRPC_un_st =
+            "client -> server;" +
+            "(server -> client)+";
+    static final String gRPC_st_un =
+            "(client -> server)+;" +
+            "server -> client";
+    static final String gRPC_st_st =
+            "client -> server ;" +
+            "( client -> server | server -> client )*";
+
+
+    public PatternDetector(CompilerContext ctx){
+        this.ctx = ctx;
+    }
+
+    public void detectCompatibleFrameworks() {
+        for (Session pattern : patterns) {
+        }
+    }
+}
