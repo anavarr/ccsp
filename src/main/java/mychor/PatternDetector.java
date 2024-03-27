035255eaf684052376df25cc865c7d3e5286b293
anavarr <artnavarro@protonmail.com>
2024-03-27 14:16:01 +0100

    remove patterns from patternDetector now that they're handled using proper parser and text files + implement a rough test to check that parsing the txt file gives correct session

--- src/main/java/mychor/PatternDetector.java
+++ src/main/java/mychor/PatternDetector.java
@@ -8,27 +8,6 @@ public class PatternDetector {
 
     List<Session> patterns;
 
-    static final String REST_PATTERN =
-            "client -> server;" +
-            "server -> client";
-
-    static final String REACTIVE_MESSAGING =
-            "subscriber -> producer ; " +
-            "producer -> subscriber; " +
-            "(subscriber -> producer | producer -> subscriber)*";
-
-    static final String gRPC_un_un =
-            "client -> server;" +
-            "server -> client";
-    static final String gRPC_un_st =
-            "client -> server;" +
-            "(server -> client)+";
-    static final String gRPC_st_un =
-            "(client -> server)+;" +
-            "server -> client";
-    static final String gRPC_st_st =
-            "client -> server ;" +
-            "( client -> server | server -> client )*";
 
 
     public PatternDetector(CompilerContext ctx){
--- src/test/java/MessagePatternParsingTest.java
+++ src/test/java/MessagePatternParsingTest.java
@@ -1,12 +1,17 @@
+import mychor.Communication;
 import mychor.MessagePatternLexer;
 import mychor.MessagePatternParser;
+import mychor.Session;
+import mychor.Utils;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.HashMap;
 
 public class MessagePatternParsingTest {
     String path_prefix = "/home/arnavarr/Documents/thesis/prog/antlr4/ccsp/src/test/antlr4/Patterns/";
@@ -34,5 +39,15 @@ public class MessagePatternParsingTest {
         var spp = testFile("pattern_sequent_error.txt");
         spp.pattern();
         assertEquals(spp.getNumberOfSyntaxErrors(), 1);
+    }
+
+    @Test
+    public void simpleExchangeGivesOneElementSession() throws IOException {
+        Session s = new Session("a", "b", new Communication(Utils.Direction.SEND, Utils.Arity.SINGLE));
+        var spp = testFile("simple_exchange.txt");
+        //turn spp into a using a MessagePatternVisitor
+        var a = new HashMap<String, Session>();
+        assertTrue(a.containsKey("SIMPLE_EXCHANGE"));
+        assertTrue(a.get("SIMPLE_EXCHANGE").equals(s));
     }
 }
