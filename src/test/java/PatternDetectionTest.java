599a8cf7db0e86e2a25726ac391c704f1f489799
anavarr <artnavarro@protonmail.com>
2024-04-10 16:08:32 +0200

    improve test for compatibility

--- src/test/java/PatternDetectionTest.java
+++ src/test/java/PatternDetectionTest.java
@@ -3,72 +3,51 @@ import mychor.Session;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PatternDetectionTest extends ProgramReaderTest{
-    @Test
-    public void threeBuyersAliceStoreCantBeImplementedUsingNRestCallsWithAliceClientNorServer() throws IOException {
-        var ctx = testFile("Three_buyer_protocol.sp").compilerCtx;
-        PatternDetector detector = new PatternDetector(ctx);
-        var cf = detector.detectCompatibleFrameworks();
-        assertFalse(frameworkIsCompatibleForSessionPeers(ctx.sessions, "alice", "store",
-                "REST_client", cf));
-        assertFalse(frameworkIsCompatibleForSessionPeers(ctx.sessions, "store","alice",
-                "REST_server", cf));
+    static Map<String, List<String>> threeBuyersCompatibilityList = new HashMap<>();
+    static Map<String, List<String>> OAuth2FragmentCompatibilityList = new HashMap<>();
+
+    static{
+        threeBuyersCompatibilityList.put("alice-store", List.of("GRPC_st_st_client", "ReactiveStreams_client"));
+        threeBuyersCompatibilityList.put("store-alice", List.of("GRPC_st_st_server", "ReactiveStreams_server"));
+        threeBuyersCompatibilityList.put("alice-bob", List.of("GRPC_un_st_client", "GRPC_st_st_client"));
+        threeBuyersCompatibilityList.put("bob-alice", List.of("GRPC_un_st_server", "GRPC_st_st_server"));
+
     }
+
     @Test
-    public void threeBuyersAliceStoreCantBeImplementedUsingNGrpcUnUnWithAliceClientNorServer() throws IOException {
-        var ctx = testFile("Three_buyer_protocol.sp").compilerCtx;
-        PatternDetector detector = new PatternDetector(ctx);
-        var cf = detector.detectCompatibleFrameworks();
-        assertFalse(frameworkIsCompatibleForSessionPeers(ctx.sessions, "alice", "store",
-                "GRPC_un_un_client", cf));
-        assertFalse(frameworkIsCompatibleForSessionPeers(ctx.sessions, "store","alice",
-                "GRPC_un_un_server", cf));
+    public void checkThreeBuyersCompatibilityList() throws IOException{
+        checkProtocolCompatibilityList("Three_buyer_protocol.sp");
     }
+
     @Test
-    public void threeBuyersAliceStoreCantBeImplementedUsingNGrpcUnStrWithAliceClientNorServer() throws IOException {
-        var ctx = testFile("Three_buyer_protocol.sp").compilerCtx;
-        PatternDetector detector = new PatternDetector(ctx);
-        var cf = detector.detectCompatibleFrameworks();
-        assertFalse(frameworkIsCompatibleForSessionPeers(ctx.sessions, "alice", "store",
-                "GRPC_un_st_client", cf));
-        assertFalse(frameworkIsCompatibleForSessionPeers(ctx.sessions, "store","alice",
-                "GRPC_un_st_server", cf));
+    public void checkOAuth2FragmentCompatibilityList() throws IOException{
+        checkProtocolCompatibilityList("OAuth2_fragment.sp");
     }
-    @Test
-    public void threeBuyersAliceStoreCantBeImplementedUsingNGrpcStrUnWithAliceClientNorServer() throws IOException {
-        var ctx = testFile("Three_buyer_protocol.sp").compilerCtx;
+
+    public void checkProtocolCompatibilityList(String protocol) throws IOException{
+        var ctx = testFile(protocol).compilerCtx;
         PatternDetector detector = new PatternDetector(ctx);
         var cf = detector.detectCompatibleFrameworks();
-        assertFalse(frameworkIsCompatibleForSessionPeers(ctx.sessions, "alice", "store",
-                "GRPC_st_un_client", cf));
-        assertFalse(frameworkIsCompatibleForSessionPeers(ctx.sessions, "store","alice",
-                "GRPC_st_un_server", cf));
-    }
-    @Test
-    public void threeBuyersAliceStoreCanBeImplementedUsingGrpcStrStrWithAliceClient() throws IOException {
-        var ctx = testFile("Three_buyer_protocol.sp").compilerCtx;
-        PatternDetector detector = new PatternDetector(ctx);
-        var cf = detector.detectCompatibleFrameworks();
-        assertTrue(frameworkIsCompatibleForSessionPeers(ctx.sessions, "alice", "store",
-                "GRPC_st_st_client", cf));
-        assertTrue(frameworkIsCompatibleForSessionPeers(ctx.sessions, "store","alice",
-                "GRPC_st_st_server", cf));
-    }
-    @Test
-    public void threeBuyersAliceStoreCanBeImplementedUsingReactiveStreamsWithAliceClient() throws IOException {
-        var ctx = testFile("Three_buyer_protocol.sp").compilerCtx;
-        PatternDetector detector = new PatternDetector(ctx);
-        var cf = detector.detectCompatibleFrameworks();
-        assertTrue(frameworkIsCompatibleForSessionPeers(ctx.sessions, "alice", "store",
-                "ReactiveStreams_client", cf));
-        assertTrue(frameworkIsCompatibleForSessionPeers(ctx.sessions, "store","alice",
-                "ReactiveStreams_server", cf));
+        threeBuyersCompatibilityList.forEach((ends, compliantFrameworks) -> {
+            var peerA = ends.split("-")[0];
+            var peerB = ends.split("-")[1];
+            for (String compliantFramework : compliantFrameworks) {
+                assertTrue(frameworkIsCompatibleForSessionPeers(ctx.sessions, peerA, peerB, compliantFramework, cf));
+            }
+            //we don't want any other framework to be compatible
+            assertEquals(getCompatibleFrameworksForEnds(ctx.sessions, peerA, peerB, cf).size(), compliantFrameworks.size());
+        });
+
     }
 
     boolean frameworkIsCompatibleForSessionPeers(List<Session> sessions, String peerA, String peerB,
@@ -78,5 +57,13 @@ public class PatternDetectionTest extends ProgramReaderTest{
         if(sessionOptional.isEmpty()) return false;
         var session = sessionOptional.get();
         return (frameworks.get(session).contains(framework));
+    }
+
+    List<String> getCompatibleFrameworksForEnds(List<Session> sessions, String peerA, String peerB, Map<Session, List<String>> frameworks){
+        var sessionOptional = sessions.stream().filter(s -> s.peerA().equals(peerA) && s.peerB().equals(peerB))
+                .findFirst();
+        if(sessionOptional.isEmpty()) return new ArrayList<>();
+        var session = sessionOptional.get();
+        return frameworks.get(session);
     }
 }
